4bab62e5444521d8e2a4fb78d53f16df741783bd
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-03 03:33:00 +0000

    Snake Move commands

--- client.js
+++ client.js
@@ -3,19 +3,29 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
+  conn.setEncoding('utf8');
+  conn.on('data', () => {
+    //console.log("Successfully connected to game server");
+  });
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: SNK');
+   // setInterval(function(){ conn.write("Move: up"); }, 3000);
 
-  return conn;
+    
+    // setTimeout(() => {
+    //   conn.write("Move: right");
+    // }, 1000)
+  });
+
+  
 }
 
 module.exports = connect;
