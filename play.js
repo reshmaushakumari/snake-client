12fd1e87d49c1bdabb5fb864dfe930288da12dc7
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-03 04:42:54 +0000

    code refactor

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const connect = require('./client');
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function(userExit) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data',userExit);
-  return stdin;
-}
-const handleUserInput = function(key) {
-  if ( key === '\u0003' ) {
-    // ctrl-c ( end of text )
-    process.exit();
-  }
-}
+const  connect = require('./client');
+const  setupInput  = require('./input');
+
 
 console.log('Connecting ...');
 connect();
-setupInput(handleUserInput);
+setupInput();
